4c17bcbb292367accde10505b0fa2197b4333fb8
Pertrichor <lize981001@gmail.com>
2023-05-15 16:28:18 +0800

    fix: 修复api未挂载到Vue原型导致的bug

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ import App from './App.vue';
 import store from './store';
 import http from './setup/http';
 import utils from './utils';
+import api from './api';
 
 Vue.prototype.$http = http;
 Vue.prototype.$utils = utils;
+Vue.prototype.$api = api;
 
 Vue.config.productionTip = false;
 
